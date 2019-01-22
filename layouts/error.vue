2e7d3edba9d94f17e2351fd30e5a27a71bbfa9b0
andrew_l <andrewlevins@gmail.com>
2019-01-22 01:02:56 -0500

    update error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found oh nooo</h1>
+    <h6>Oh nooo :(</h6>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
     <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <nuxt-link to="/">You can go back home</nuxt-link>
+    <h6>or</h6>
+    <nuxt-link to="/work">You can go to my work</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
   props: ['error'],
-  layout: 'default' // you can set a custom layout for the error page
+  layout: 'default'
 }
 </script>
+
+<style scoped>
+.container {
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 2rem;
+  text-align: center;
+}
+</style>
