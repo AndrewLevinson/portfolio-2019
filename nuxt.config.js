5d7da3c868f181c0e53a260ab6b70b26b2ac5ac8
andrew_l <andrewlevins@gmail.com>
2019-05-22 01:53:46 -0400

    add water route in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ module.exports = {
     routes: function() {
       return [
         '/work',
+        '/projects/water',
         '/projects/cookies',
         '/projects/ease-energy',
         '/projects/arc',
